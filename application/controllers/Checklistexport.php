4a736749afb9b407208c6180fd089f020044f322
wgk <williamgkeen@gmail.com>
2015-03-18 15:50:48 -0500

    Added comments for basic function flow

--- application/controllers/Checklistexport.php
+++ application/controllers/Checklistexport.php
@@ -1,8 +1,25 @@
 <?php
-class checklistexport extends CI_Controller
+class Checklistexport extends CI_Controller
 {
 	public function index()
 	{
 		echo 'Hello World!';
+	}
+
+	public function xls($userobject)
+	{
+		//Assuming a userobject with classes is passed
+		//	Must be valid!
+		
+		//Parse classes
+		//	Curriculum in database
+		//		Has associated slots on the curriculum
+		//		Curriculum slots have valid classes associated that can fill those slots
+		//	User has list of taken classes in an array
+		//	All classes are simply plain text names
+
+		//Create file object (plaintext?)
+
+		//Return file object (PDF or XLS)
 	}
 }
