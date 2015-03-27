e8657a5455b656932ea71f00d949672dedf99aae
wgk <williamgkeen@gmail.com>
2015-03-27 14:32:11 -0500

    Undoing a change to welcome_message for pull to dev

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -85,13 +85,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
-<h1>BUTTON FOR TESTING EXPORT</h1>
-
-<?php
-echo '<button type="button">';
-echo "<a href=\"index.php/checklistexport/index/10210078\">Download</a>";
-echo '</button>';
-?>
-
 </body>
 </html>
