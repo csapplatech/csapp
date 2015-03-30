09e6c1aebf4bb9a71bf5881c477236c38da9112e
Regan Carver <lordcyrus8229@gmail.com>
2015-03-30 14:58:41 -0500

    Fixed bugs with incorrect capitalization on variable calls

--- application/models/Course_model.php
+++ application/models/Course_model.php
@@ -141,10 +141,10 @@ class Course_model extends CI_Model
 	{
 		$models = array();
 		
-		if($this->CourseID != null)
+		if($this->courseID != null)
 		{
 			$this->db->select('RequisiteCourseID');
-			$this->db->where('CourseID', $this->CourseID);
+			$this->db->where('CourseID', $this->courseID);
 			
 			$results = $this->db->get('CourseRequisites');
 			
@@ -172,10 +172,10 @@ class Course_model extends CI_Model
 	{
 		$models = array();
 		
-		if($this->CourseID != null)
+		if($this->courseID != null)
 		{
 			$this->db->select('RequisiteCourseID');
-			$this->db->where('CourseID', $this->CourseID);
+			$this->db->where('CourseID', $this->courseID);
 			
 			$results = $this->db->get('CourseRequisites');
 			
@@ -190,7 +190,7 @@ class Course_model extends CI_Model
 			}
 			
 			$this->db->select('CourseID');
-			$this->db->where('RequisiteCourseID', $this->CourseID);
+			$this->db->where('RequisiteCourseID', $this->courseID);
 			
 			$results = $this->db->get('CourseRequisites');
 			
