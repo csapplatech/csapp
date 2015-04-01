2711042d89b49c468b8f036898aac319fb395f60
Troutzorz <azriel_richardson@yahoo.com>
2015-04-01 14:22:25 -0500

    Guest Login

--- index.php
+++ index.php
@@ -291,7 +291,7 @@ switch (ENVIRONMENT)
  */
 session_start();
 if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 900)) {
-    // last request was more than 30 minutes ago
+    // last request was more than 15 minutes ago
     session_unset();     // unset $_SESSION variable for the run-time 
     session_destroy();   // destroy session data in storage
     $_SESSION = array();
