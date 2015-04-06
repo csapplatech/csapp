0d58f4b809152a506c4fc0702133105d38135ac5
wgk <williamgkeen@gmail.com>
2015-04-06 01:46:53 -0500

    Fixed the checklist export.  Advisor checklist SHOULD be almost finished. It doesn't fill in the academic years yet

--- application/controllers/Checklistexport.php
+++ application/controllers/Checklistexport.php
@@ -67,12 +67,13 @@ class Checklistexport extends CI_Controller
 	    $advlist->getStyle("A1:P14")->getFont()->setBold(true);
 	    $advlist->getStyle("A1:P2")->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
 	    $advlist->getStyle("A1:P2")->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
+	    $advlist->getStyle("A1:A14")->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
 	 
 	    //Set background for header cells
 	    $advlist->getStyle("B1:P2")->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID)
-	    					  ->getStartColor()->setRGB('D8D8D8');
+	    					  ->getStartColor()->setRGB('EEEEEE');
 	    $advlist->getStyle("A3:A14")->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID)
-	    					   ->getStartColor()->setRGB('D8D8D8');
+	    					   ->getStartColor()->setRGB('EEEEEE');
 
 	    //Set up top row (header) cells
 	    $advlist->mergeCells("A1:A2");
@@ -91,27 +92,33 @@ class Checklistexport extends CI_Controller
 	    $advlist->getCell("A3")->SetValue("The term, year, and grade of courses already taken have been updated on the check sheet.");
 	    //Style needed for EVERY (9) following rich text run
 	    $fontstyle = new PHPExcel_Style_Font();
-	    $fontstyle->setName("Ariel")->setSize(10);
+	    $fontstyle->setName("Arial")->setSize(10)->setBold(true);
 	    //Cell A4 -- A lot of code to make a single word green
 	    $text = new PHPExcel_RichText();
-	    $run1 = $text->createTextRun("The term and year of current (ongoing) courses have been highlighted in ")->setFont($fontstyle);
-	    $run2 = $text->createTextRun("green")->setFont($fontstyle);
-	    $run2->getFont()->setColor(new PHPExcel_Style_Color(PHPExcel_Style_Color::COLOR_GREEN));
-	    $run3 = $text->createTextRun(" on the check sheet.")->setFont($fontStyle);
+	    $run1 = $text->createTextRun("The term and year of current (ongoing) courses have been highlighted in ");
+	    $run1->setFont($fontstyle);
+	    $run2 = $text->createTextRun("green");
+	    $run2->getFont()->setColor(new PHPExcel_Style_Color(PHPExcel_Style_Color::COLOR_GREEN))->setName("Arial")->setSize(10)->setBold(true);
+	    $run3 = $text->createTextRun(" on the check sheet.");
+	    $run3->setFont($fontstyle);
 	    $advlist->getCell("A4")->SetValue($text);
 	    //Cell A5 -- A lot of code to make a single word yellow
 	    $text = new PHPExcel_RichText();
 	    $run1 = $text->createTextRun("The term and year of courses scheduled for the next term are highlighted in ");
+	    $run1->setFont($fontstyle);
 	    $run2 = $text->createTextRun("yellow");
-	    $run2->getFont()->setColor(new PHPExcel_Style_Color(PHPExcel_Style_Color::COLOR_YELLOW));
+	    $run2->getFont()->setColor(new PHPExcel_Style_Color(PHPExcel_Style_Color::COLOR_YELLOW))->setName('Arial')->setSize(10)->setBold(true);
 	    $run3 = $text->createTextRun(" on the check sheet.");
+	    $run3->setFont($fontstyle);
 	    $advlist->getCell("A5")->SetValue($text);
 	    //Cell A6 -- A lot of code to make a single word red
 	    $text = new PHPExcel_RichText();
 	    $run1 = $text->createTextRun("Problems have been highlighted in ");
+	    $run1->setFont($fontstyle);
 	    $run2 = $text->createTextRun("red");
-	    $run2->getFont()->setColor(new PHPExcel_Style_Color(PHPExcel_Style_Color::COLOR_RED));
+	    $run2->getFont()->setColor(new PHPExcel_Style_Color(PHPExcel_Style_Color::COLOR_RED))->setName('Arial')->setSize(10)->setBold(true);
 	    $run3 = $text->createTextRun(" on the check sheet and have been discussed with the student.");
+	    $run3->setFont($fontstyle);
 	    $advlist->getCell("A6")->SetValue($text);
 	    $advlist->getCell("A7")->SetValue("All course substitutions have been approved either in the system (BOSS/CICS) or by the Program Chair.");
 	    $advlist->getCell("A8")->SetValue("The student has taken all COES prerequisites (and earned a 'C' or better) for all COES courses on the advisement sheet.");
@@ -122,18 +129,21 @@ class Checklistexport extends CI_Controller
 	    $advlist->getCell("A13")->SetValue("The check sheet has been uploaded/copied to the cloud space.");
 	    $advlist->getCell("A14")->SetValue("The student has been released on BOSS or CICS screen 7R3.");
 	    
-	    //There's an issue with excel
-	    //	A row can't have autosized height, so it must be hard coded
-	    $advlist->getRowDimension(3)->setRowHeight(12*2+1);
-	    $advlist->getRowDimension(4)->setRowHeight(12*2+1);
-	    $advlist->getRowDimension(5)->setRowHeight(12*2+1);
-	    $advlist->getRowDimension(6)->setRowHeight(12*2+1);
-	    $advlist->getRowDimension(7)->setRowHeight(12*2+1);
-	    $advlist->getRowDimension(8)->setRowHeight(12*2+1);
-	    $advlist->getRowDimension(9)->setRowHeight(12*2+1);
-	    $advlist->getRowDimension(10)->setRowHeight(12*2+1);
-	    $advlist->getRowDimension(11)->setRowHeight(12*2+1);
-	    $advlist->getRowDimension(12)->setRowHeight(12*3+1);
+	    //Set row heights
+	    //  There's an issue with excel
+	    //	  A row can't have autosized height, so it must be hard coded
+	    $advlist->getRowDimension(3)->setRowHeight(14*2);
+	    $advlist->getRowDimension(4)->setRowHeight(14*2);
+	    $advlist->getRowDimension(5)->setRowHeight(14*2);
+	    $advlist->getRowDimension(6)->setRowHeight(14*2);
+	    $advlist->getRowDimension(7)->setRowHeight(14*2);
+	    $advlist->getRowDimension(8)->setRowHeight(14*2);
+	    $advlist->getRowDimension(9)->setRowHeight(14*2);
+	    $advlist->getRowDimension(10)->setRowHeight(14*2);
+	    $advlist->getRowDimension(11)->setRowHeight(14*2);
+	    $advlist->getRowDimension(12)->setRowHeight(14*3);
+	    $advlist->getRowDimension(13)->setRowHeight(14*1);
+	    $advlist->getRowDimension(14)->setRowHeight(14*1);
 	}
 
 	//Generate Course Checklist
