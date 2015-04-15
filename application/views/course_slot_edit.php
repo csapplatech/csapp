4662d3688e98e72c6126b58350f774b5628bab6d
wgk <williamgkeen@gmail.com>
2015-04-15 14:23:45 -0500

    Commiting changes

--- application/views/course_slot_edit.php
+++ application/views/course_slot_edit.php
@@ -1,2 +1,16 @@
-<html>
-</html>
+<h1>Course Edit</h1>
+
+<select multiple size='3'>
+<option>Test</option>
+<option>Test</option>
+<option>Test</option>
+<option>Test</option>
+<option>Test</option>
+<?php /*
+foreach($query->result_array() as $row)
+	echo "<option> $row </option>";
+*/ ?>
+</select>
+<br \>
+<button onclick="location.href='<?php echo site_url('Mainpage/index'); ?>'">Save</button>
+<button onclick="location.href='<?php echo site_url('Mainpage/index'); ?>'">Cancel</button>
