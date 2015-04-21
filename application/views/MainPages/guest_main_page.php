b071f1f23526bd17a092bd2e99d17adcb990c613
Troutzorz <azriel_richardson@yahoo.com>
2015-04-21 11:37:44 -0500

    Added timestamping on login
    Made the footer a template file to be included in each view
    
    Cleaned up the views a bit

--- application/views/MainPages/guest_main_page.php
+++ application/views/MainPages/guest_main_page.php
@@ -9,7 +9,6 @@
         <meta name="author" content="">
         <link rel="icon" href="<?php echo IMG.'/icon.ico'; ?>">
         <link rel="stylesheet" href="<?php echo CSS.'/magic-bootstrapV2_1.css'; ?>" type="text/css">
-        <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
     </head>
     <body style="padding-top: 60px">
         
@@ -37,20 +36,13 @@
 
         <h3> Welcome to the Guest Main Page, <?php echo $user->getName(); ?>!</h3>
         
-        <p><img src= <?php echo IMG."/tech_official_logo.jpg" ?> alt="Tech Logo" style="width:500px;height:425px"></p>
-        
-        <footer>
-            <font color ="white"</font>
-            <p>Ruston, LA 2015</p>
-            <p>Louisiana Tech University</p>
-            <p>Created by: Louisiana Tech's CSC 404 Class</p>
-        </footer>
+        <p><img src= "<?php echo IMG."/tech_official_logo.jpg" ?>" alt="Tech Logo" style="width:500px;height:425px"></p>
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-        <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+        
+        <?php include_once($_SERVER['DOCUMENT_ROOT'].'/application/views/Templates/footer.php'); ?>
         
     </body>
 </html>
--- application/views/MainPages/student_main_page.php
+++ application/views/MainPages/student_main_page.php
@@ -9,7 +9,6 @@
         <meta name="author" content="">
         <link rel="icon" href="<?php echo IMG.'/icon.ico'; ?>">
         <link rel="stylesheet" href="<?php echo CSS.'/magic-bootstrapV2_1.css'; ?>" type="text/css">
-        <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
     </head>
     <body style="padding-top: 60px">
         
@@ -67,20 +66,13 @@
       
         <!-- change the site uri to your specific uri part -->
         
-        <p> <img src= <?php echo IMG."/tech_official_logo.jpg" ?> alt="Tech Logo" style="width:300px;height:225px"> </p>
-            
-        <footer>
-            <font color ="white"</font>
-            <p>Ruston, LA 2015</p> 
-            <p>Louisiana Tech University</p>
-            <p>Created by: Louisiana Tech's CSC 404 Class</p>
-        </footer>
+        <p> <img src= "<?php echo IMG."/tech_official_logo.jpg" ?>" alt="Tech Logo" style="width:300px;height:225px"> </p>
         
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-        <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+        
+        <?php include_once($_SERVER['DOCUMENT_ROOT'].'/application/views/Templates/footer.php'); ?>
         
     </body>
 </html>
