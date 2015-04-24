4a65d4b3d4950291921e05e2680e90a11f05a77d
wgk <williamgkeen@gmail.com>
2015-04-23 22:54:41 -0500

    Increased Password Complexity

--- application/controllers/Activation.php
+++ application/controllers/Activation.php
@@ -19,6 +19,8 @@ class Activation extends CI_Controller
 
 		//Array of characters to generate password
 		$charset = array(
+		        '!','@','#','$','%','^','&','*','(',')',
+			'~','=','+','_','-','?','/','>','<','.',
 			'0','1','2','3','4','5','6','7','8','9',
 			'a','b','c','d','e','f','g','h','i','j',
 			'k','l','m','n','o','p','q','r','s','t',
